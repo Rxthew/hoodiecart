510852622fd2e279cca0ca0ff29df41864e985e9
rxthew <richardtheumaster@gmail.com>
2024-01-16 18:14:48 +0100

    Import router link

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../services/auth.service';
-import { Router, NavigationEnd } from '@angular/router';
+import { Router, NavigationEnd, RouterLink } from '@angular/router';
 
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
